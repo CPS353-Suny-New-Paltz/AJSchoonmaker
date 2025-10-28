9184a091ddfbeafd3ee696cd0eda3d9d6f245122
AJSchoonmaker <anthonyjosephschoonmaker@gmail.com>
2025-10-28 13:36:52 -0400

    Exampl check (#25)
    * Replace greeting message with a longer text
    
    * Update output message in BadHello.java

--- src/example/BadHello.java
+++ src/example/BadHello.java
@@ -2,6 +2,6 @@ package example;
 
 public class BadHello {
 		public static void main(String[] args){
-			System.out.println("Hello, World.... :D ");
+			System.out.println("Hello worldo :D");
 		}
 }
